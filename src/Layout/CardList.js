2a2998c682855fd810261b95bd04787146bf3b7e
Cannon Spears <cannonspears@gmail.com>
2023-04-28 11:50:31 -0500

    Implemented Deck Study

--- src/Layout/CardList.js
+++ src/Layout/CardList.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 
+function CardList({deck}) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isCardShown, setIsCardShown] = useState(true);
 
-function CardList({card}) {
-  
-  // {deck.cards?.map(card => {
-  //         return
-  //       })}
+  const currentCard = deck.cards ? deck.cards[currentIndex] : null;
 
-  return <div>
-    <p>{card.front}</p>
-    <p>{card.back}</p>
-  </div>
+  const handleFlip = () => setIsCardShown(!isCardShown);
+
+  const handleNext = () => {
+    if (currentIndex < deck.cards.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      setIsCardShown(true);
+    } else {
+      const restart = window.confirm(
+        "Restart cards?\n\nClick 'Cancel' to return to the home page."
+      );
+      if (restart) {
+        setCurrentIndex(0);
+        setIsCardShown(true);
+      } else {
+        window.location.href = "/";
+      }
+    }
+  };
+
+  return (
+    <div>
+      {currentCard ? (
+        
+        <div class="card">
+          <div class="card-body">
+            <h5>Card {currentIndex + 1} of {deck.cards.length}</h5>
+            <p class="card-text">{isCardShown ? currentCard.front : currentCard.back}</p>
+            <button type="button" className="btn btn-secondary mr-2" onClick={handleFlip}>Flip</button>
+            {isCardShown ? null : <button type="button" className="btn btn-primary" onClick={handleNext}>Next</button>}
+          </div>
+        </div>
+        
+        
+        
+      ) : (
+        <p>There are no cards in this deck.</p>
+      )}
+    </div>
+  );
 }
 
 export default CardList
--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -3,22 +3,49 @@ import { useParams, Link } from 'react-router-dom'
 import { readDeck } from '../utils/api';
 import CardList from './CardList';
 
-function Study() {
-    const {deckId} = useParams()
-    const [deck, setDeck] = useState({})
-    console.log("deck", deck)
 
-    useEffect(() => {
-      readDeck(deckId).then((data) => setDeck(data));
-      }, [deckId]);
+
+
+function Study() {
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState({});
+  const [notEnoughCards, setNotEnoughCards] = useState(false);
+
+  useEffect(() => {
+    readDeck(deckId).then((data) => {
+      if (data.cards.length < 3) {
+        setNotEnoughCards(true);
+      }
+      setDeck(data);
+    });
+  }, [deckId]);
 
   return (
     <Fragment>
-      <Link to="/" type="btn" className="btn btn-primary">Go Home</Link>
-      <h1>Study: Rendering in React</h1>
-        <CardList deck={deck}/>
+       <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+        <li class="breadcrumb-item"><Link to={`/decks/${deckId}`}>{deck.name}</Link></li>
+        <li class="breadcrumb-item active" aria-current="page">Study</li>
+      </ol>
+    </nav>
+    <h1>{deck.name}: Study</h1>
+      {notEnoughCards ? (
+        <Fragment>
+          <h2>Not enough cards.</h2>
+          <p>You need at least 3 cards to study. There are {deck?.cards?.length} cards in this deck.</p>
+          <Link to={`/decks/${deckId}/cards/new`}>
+            <button type="button" className="btn btn-primary">Add Cards</button>
+          </Link>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <CardList deck={deck} />
+        </Fragment>
+      )}
     </Fragment>
-  )
+  );
 }
+
 
 export default Study
