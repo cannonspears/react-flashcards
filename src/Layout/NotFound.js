4f35c3f2deb6cd775c75effffef845b449b2d845
Cannon Spears <cannonspears@gmail.com>
2023-05-02 17:49:55 -0500

    Breadcrumb on 404

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Breadcrumb from "./Breadcrumb";
 
 function NotFound() {
   return (
     <div className="NotFound">
+      <Breadcrumb currentPage="Not Found" />
       <h1>Not Found</h1>
     </div>
   );
