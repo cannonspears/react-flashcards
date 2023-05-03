c18247b594df8ea9279fd8769cc6d46a92db07c1
Cannon Spears <cannonspears@gmail.com>
2023-05-03 08:52:27 -0500

    Removed Breadcrumb to pass tests

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Breadcrumb from "./Breadcrumb";
 
 function NotFound() {
   return (
     <div className="NotFound">
-      <Breadcrumb currentPage="Not Found" />
       <h1>Not Found</h1>
     </div>
   );
